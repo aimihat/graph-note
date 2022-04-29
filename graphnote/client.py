fd71a9acea69333b148511a60ab0175bf851f2b6
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-04-29 03:05:17 +0300

    executing dag and displaying cell outputs

--- graphnote/client.py
+++ graphnote/client.py
@@ -1,35 +1,57 @@
-import jupyter_client
+import asyncio
 import dataclasses
 from pathlib import Path
+from typing import Any
 
-CELL_DIR = Path(__file__).parent.parent / 'cells'
-print(CELL_DIR, CELL_DIR.parent, CELL_DIR.parent.parent)
+import jupyter_client
+
+import message_parser
+import messages
+from dags import Node
+
+CELL_DIR = Path(__file__).parent.parent / "cells"
 KERNEL_SPEC_DIR = Path("/Users/aimilioshatzistamou/Library/Jupyter/runtime/")
 
 client = jupyter_client.BlockingKernelClient()
 
 # Get connection info
-client.load_connection_file(KERNEL_SPEC_DIR / "kernel-69224.json")
+client.load_connection_file(str(KERNEL_SPEC_DIR / "kernel-72378.json"))
 client.start_channels()
 
-@dataclasses.dataclass
-class Node:
-    filepath: str
 
-    def load_code(self):
-        with open(self.filepath, 'r') as f:
-            return f.read()
+class GraphRunner:
+    def __init__(self, dag):
+        self.dag = dag
+        self.parser = message_parser.MessageParser()
 
-dag = [
-    Node(CELL_DIR / "node_root.py"),
-    Node(CELL_DIR / "node_data.py"),
-    Node(CELL_DIR / "node_trainer.py"),
-    Node(CELL_DIR / "node_visualize.py"),
-]
+    def display_execution_output(self, msg: Any) -> None:
+        parsed_message = self.parser.parse_message(msg)
+        if type(parsed_message.content) == messages.CellStdout:
+            print(parsed_message.content.text)
 
-# Execute dag
-for node in dag:
-    print(f"Executing {node.filepath}")
-    reply_msg = client.execute(node.load_code())
-    reply = client.get_shell_msg()
-    print(reply)
+    async def run_node(self, node):
+        await client._async_execute_interactive(
+            node.code, output_hook=self.display_execution_output
+        )
+
+    async def run_dag_in_order(self):
+        for node in self.dag:
+            print(
+                f"Executing {node.filepath}, with inputs {node.input_vars}, and outputs {node.output_vars}"
+            )
+            await self.run_node(node)
+
+
+async def main():
+    dag = [
+        Node(CELL_DIR / "node_root.py"),
+        Node(CELL_DIR / "node_data.py"),
+        Node(CELL_DIR / "node_trainer.py"),
+        Node(CELL_DIR / "node_visualize.py"),
+    ]
+
+    runner = GraphRunner(dag)
+    await runner.run_dag_in_order()
+
+
+asyncio.run(main())
