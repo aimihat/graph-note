855b86996761e799a8661fae8bbc9160d4528c63
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-04-29 03:04:32 +0300

    message & content classes

---
+++ graphnote/messages.py
@@ -0,0 +1,25 @@
+import dataclasses
+from typing import Any, Dict, Union
+
+### Types: Message Content ###
+
+
+@dataclasses.dataclass
+class CellStdout:
+    name: str
+    text: str
+
+
+RawContent = Dict[str, Any]
+
+Content = Union[RawContent, CellStdout]
+
+
+@dataclasses.dataclass
+class Message:
+    header: Dict
+    msg_id: str
+    msg_type: str
+    parent_header: Dict
+    metadata: Dict
+    content: Content
