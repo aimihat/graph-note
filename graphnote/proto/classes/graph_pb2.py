7d9c6b9f8947d052e6c35a7452036bb15c795453
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-05-08 01:47:44 +0100

    feat: small update to proto definition

--- graphnote/proto/classes/graph_pb2.py
+++ graphnote/proto/classes/graph_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bgraph.proto\"!\n\x04Port\x12\x0b\n\x03uid\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\"4\n\nConnection\x12\x13\n\x04\x66rom\x18\x01 \x01(\x0b\x32\x05.Port\x12\x11\n\x02to\x18\x02 \x01(\x0b\x32\x05.Port\"t\n\x04\x43\x65ll\x12\x0b\n\x03uid\x18\x01 \x01(\t\x12\x0c\n\x04\x63ode\x18\x02 \x01(\t\x12\x17\n\x08in_ports\x18\x03 \x03(\x0b\x32\x05.Port\x12\x18\n\tout_ports\x18\x04 \x03(\x0b\x32\x05.Port\x12\x13\n\x06output\x18\x05 \x01(\tH\x00\x88\x01\x01\x42\t\n\x07_output\"T\n\x05Graph\x12\x13\n\x04root\x18\x01 \x01(\x0b\x32\x05.Cell\x12\x14\n\x05\x63\x65lls\x18\x02 \x03(\x0b\x32\x05.Cell\x12 \n\x0b\x63onnections\x18\x03 \x03(\x0b\x32\x0b.Connectionb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bgraph.proto\"!\n\x04Port\x12\x0b\n\x03uid\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\">\n\nConnection\x12\x18\n\tfrom_port\x18\x01 \x01(\x0b\x32\x05.Port\x12\x16\n\x07to_port\x18\x02 \x01(\x0b\x32\x05.Port\"t\n\x04\x43\x65ll\x12\x0b\n\x03uid\x18\x01 \x01(\t\x12\x0c\n\x04\x63ode\x18\x02 \x01(\t\x12\x17\n\x08in_ports\x18\x03 \x03(\x0b\x32\x05.Port\x12\x18\n\tout_ports\x18\x04 \x03(\x0b\x32\x05.Port\x12\x13\n\x06output\x18\x05 \x01(\tH\x00\x88\x01\x01\x42\t\n\x07_output\"T\n\x05Graph\x12\x13\n\x04root\x18\x01 \x01(\x0b\x32\x05.Cell\x12\x14\n\x05\x63\x65lls\x18\x02 \x03(\x0b\x32\x05.Cell\x12 \n\x0b\x63onnections\x18\x03 \x03(\x0b\x32\x0b.Connectionb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'graph_pb2', globals())
@@ -23,9 +23,9 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _PORT._serialized_start=15
   _PORT._serialized_end=48
   _CONNECTION._serialized_start=50
-  _CONNECTION._serialized_end=102
-  _CELL._serialized_start=104
-  _CELL._serialized_end=220
-  _GRAPH._serialized_start=222
-  _GRAPH._serialized_end=306
+  _CONNECTION._serialized_end=112
+  _CELL._serialized_start=114
+  _CELL._serialized_end=230
+  _GRAPH._serialized_start=232
+  _GRAPH._serialized_end=316
 # @@protoc_insertion_point(module_scope)
