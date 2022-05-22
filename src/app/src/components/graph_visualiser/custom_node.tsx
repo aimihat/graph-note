9d1a9f1401251b3184ab86db98eb588aa9b1e88f
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-05-22 21:22:59 +0200

    refactor: graph component

---
+++ src/app/src/components/graph_visualiser/custom_node.tsx
@@ -0,0 +1,85 @@
+import { memo } from "react";
+import { Handle, HandleType, Position } from "react-flow-renderer";
+import { NodeDataType, PortType } from "../../types";
+
+interface CustomNodeProps {
+  data: NodeDataType;
+}
+
+interface CustomHandleProps {
+  type: HandleType;
+  numSameTypePorts?: number;
+  order: number;
+  port: PortType;
+  position: Position;
+}
+
+function CustomHandle({
+  type,
+  numSameTypePorts,
+  port,
+  order,
+  position,
+}: CustomHandleProps) {
+  let topPosition = 0;
+  if (numSameTypePorts !== undefined) {
+    topPosition = 20 + (order * (80 - 20)) / (numSameTypePorts - 1);
+  }
+
+  return (
+    <Handle
+      type={type}
+      style={{ top: String(topPosition) + "%" }}
+      id={port.uid}
+      isConnectable={true}
+      position={position}
+    />
+  );
+}
+
+export default memo(({ data }: CustomNodeProps) => {
+  let numOutputPorts = data.outputPorts?.length;
+  let outPortHandles = data.outputPorts?.map(
+    (port: PortType, index: number) => {
+      return (
+        <CustomHandle
+          type="source"
+          key={port.uid}
+          numSameTypePorts={numOutputPorts}
+          port={port}
+          position={Position.Right}
+          order={index}
+        />
+      );
+    }
+  );
+
+  let numInputPorts = data.inputPorts?.length;
+  let inPortHandles = data.inputPorts?.map((port: any, index: number) => {
+    return (
+      <CustomHandle
+        type="target"
+        key={port.uid}
+        port={port}
+        numSameTypePorts={numInputPorts}
+        position={Position.Left}
+        order={index}
+      />
+    );
+  });
+
+  // compute cell size based on number of ports
+  let maxVerticalPorts = Math.max(numInputPorts ?? 0, numOutputPorts ?? 0);
+  let cellSize = { lineHeight: maxVerticalPorts };
+
+  return (
+    <div className="nodeParent">
+      <div className="custom-node" style={cellSize}>
+        <div className="inputPorts">{inPortHandles}</div>
+        <div>{data.label}</div>
+        <div className="outputPorts">{outPortHandles}</div>
+      </div>
+      <div className="nodeFooter">Cell status</div>
+    </div>
+  );
+});
---
+++ src/app/src/components/graph_visualiser/graph.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import ReactFlow, {
+  Controls,
+  MiniMap,
+  Node,
+  ReactFlowInstance,
+} from "react-flow-renderer";
+import DAGNode from "./custom_node";
+
+import "./node.css";
+import { GraphType, SetGraphType } from "../../types";
+import { deserialize_graph } from "../../utils/protobuf_utils";
+
+const rfStyle = {
+  backgroundColor: "#B8CEFF",
+};
+
+const nodeTypes = {
+  dagNode: DAGNode,
+};
+
+interface FlowProps {
+  setSelectedNodeId: (arg0: string) => void;
+  setGraph: SetGraphType;
+  graph?: GraphType;
+}
+
+function Flow({ setSelectedNodeId, graph, setGraph }: FlowProps) {
+  const [reactFlowInstance, setReactFlowInstance] =
+    useState<ReactFlowInstance | null>();
+
+  const onInit = (rf: ReactFlowInstance) => {
+    setReactFlowInstance(rf);
+
+    fetch("http://localhost:8000/read_graph")
+      .then((r) => r.arrayBuffer())
+      .then((serialised_graph) => {
+        deserialize_graph(
+          serialised_graph,
+          function (deserialised_graph: GraphType) {
+            setGraph((_) => deserialised_graph);
+          }
+        );
+      })
+      .catch((error) => console.log(error));
+  };
+
+  function updateSelectedNode(_: React.MouseEvent, node: Node) {
+    setSelectedNodeId(node.id);
+  }
+
+  useEffect(() => {
+    if (reactFlowInstance) {
+      reactFlowInstance.fitView();
+    }
+  }, [graph?.nodes.length]);
+
+  return (
+    <ReactFlow
+      nodes={graph?.nodes}
+      edges={graph?.edges}
+      nodeTypes={nodeTypes}
+      style={rfStyle}
+      onInit={onInit}
+      onNodeClick={updateSelectedNode}
+      fitView
+    >
+      <MiniMap />
+      <Controls />
+    </ReactFlow>
+  );
+}
+
+export default Flow;
