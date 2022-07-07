25afc579912223ce606428ea1620cdcc8a283685
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-07-07 23:30:28 +0100

    remove: test_notebook

--- test_notebook/test_cells/cell_data.py
+++
@@ -1,3 +0,0 @@
-digits = datasets.load_digits()
-
-graphnote.out({"digits__different_name": digits})
--- test_notebook/test_cells/cell_root.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pyplot as plt
-# Import datasets, classifiers and performance metrics
-from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
