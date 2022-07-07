f6c93373b857bf63b2e9fa58a6caba62af5147fe
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-07-07 21:25:07 +0100

    update cell output after running cell

--- execution/runner.py
+++ execution/runner.py
@@ -4,12 +4,11 @@ from typing import Any
 
 import jupyter_client
 
-from graphnote.execution.helpers.code_helpers import compile_cell
-from graphnote.execution.helpers.graph_helpers import (validate_cell,
-                                                       validate_root)
-from graphnote.execution.messages import definitions, parsers
-from graphnote.kernel import initialization
-from graphnote.proto.classes import graph_pb2
+from execution.helpers.code_helpers import compile_cell
+from execution.helpers.graph_helpers import validate_cell, validate_root
+from execution.messages import definitions, parsers
+from kernel import initialization
+from proto.classes import graph_pb2
 
 
 class GraphExecutor:
@@ -22,13 +21,15 @@ class GraphExecutor:
 
     async def init_kernel(self):
         # TODO: this shouldn't be done here.
+        self.logger.info("Initializing kernel.")
         await self.client._async_execute_interactive(inspect.getsource(initialization))
 
     async def run_root(self, root: graph_pb2.Cell) -> None:
+        logging.info("Executing root node.")
         if validate_root(root):
             self.logger.info(f"Executing root node.")
             reply = await self.client._async_execute_interactive(
-                root.code, output_hook=self.display_execution_output
+                root.code  # , output_hook=self.display_execution_output
             )
             if reply["content"]["status"] == "error":
                 traceback = reply["content"]["traceback"]
@@ -43,20 +44,20 @@ class GraphExecutor:
         )
         if validate_cell(self.dag, cell):
             exec_code = compile_cell(self.dag, cell)
-            reply = await self.client._async_execute_interactive(
-                exec_code, output_hook=self.display_execution_output
+
+            update_cell_output_ = lambda msg: self.update_cell_output(cell, msg)
+
+            await self.client._async_execute_interactive(
+                exec_code, output_hook=update_cell_output_
             )
-            # TODO: convert this to use message interface.
-            if reply["content"]["status"] == "error":
-                traceback = reply["content"]["traceback"]
-                for trace in traceback:
-                    print(trace)
         else:
             raise Exception("The cell is not valid and cannot be run.")
 
-    def display_execution_output(self, msg: Any) -> None:
+    def update_cell_output(self, cell, msg):
         parsed_message = parsers.parse_message(msg)
         if type(parsed_message.content) == definitions.CellStdout:
-            print(parsed_message.content.text)
+            cell.output = parsed_message.content.text
         elif type(parsed_message.content) == definitions.CellStderr:
-            print(parsed_message.content.text)
+            cell.output = parsed_message.content.text
+        else:
+            logging.warning("Unknown output type.")
