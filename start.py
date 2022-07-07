d288e06c95b67e4695ba42090ef6e0b143f8409f
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-07-07 21:22:12 +0100

    start script

---
+++ start.py
@@ -0,0 +1,17 @@
+import kernel.launcher as kernel_launcher
+import uvicorn
+from api.main import app
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    # Start a child process to run the kernel.
+    kernel_launcher.start_kernel()
+
+    # Start a child process to run the UI.
+    subprocess.Popen(["npm","start","--prefix","app"])
+
+    # Start the FastAPI server.
+    uvicorn.run(app, host="0.0.0.0", port=8000)
