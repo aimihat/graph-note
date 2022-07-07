027f7849a87ee9f2faa8d806409c354fd0cf3de3
Aimilios Hatzistamou <aimilios@hatzistamou.com>
2022-07-07 21:25:53 +0100

    handle updated graph part 2

--- api/main.py
+++ api/main.py
@@ -8,51 +8,41 @@ POST /run_cell (cell_id) -> runs the cell & returns updated cell (with output).
 future: run full dag, with websocket updates.
 """
 
-from typing import Optional
-
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, File, Response, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from execution.helpers.graph_helpers import detect_in_ports
+from kernel.launcher import CONNECT_FILE_PATH
+import logging
+from pydantic import BaseModel
+import base64
+from google.protobuf.json_format import MessageToJson
 
+logger = logging.getLogger()
 app = FastAPI()
 
 import asyncio
-import logging
-from pathlib import Path
 
 import jupyter_client
-
 from execution.runner import GraphExecutor
 from proto.classes import graph_pb2
 
-KERNEL_SPEC_DIR = Path(__file__).parent.parent.parent / ".kernelspecs"
-CONNECT_FILE_PATH = KERNEL_SPEC_DIR / Path("kernel.json")
-
-logging.basicConfig(level=logging.INFO)
 client = jupyter_client.BlockingKernelClient()
 client.load_connection_file(str(CONNECT_FILE_PATH))
 client.start_channels()
 
+DAGBOOK_PATH = "dagbooks/test_dag.gnote"
 
-def get_cell(graph, cell_id):
-    ...
+# Initialize the graph runner.
+with open(DAGBOOK_PATH, "rb") as f:
+    test_graph = graph_pb2.Graph()
+    test_graph.ParseFromString(f.read())
 
+runner = GraphExecutor(client, test_graph)
 
-async def main():
-    with open("test_notebook/test_dag.gnote", "rb") as f:
-        test_graph = graph_pb2.Graph()
-        test_graph.ParseFromString(f.read())
-    runner = GraphExecutor(client, test_graph)
+# Prepare the kernel for execution.
+asyncio.run(runner.init_kernel())
+asyncio.run(runner.run_root(test_graph.root))
 
-    await runner.init_kernel()
-
-    await runner.run_root(test_graph.root)
-
-    # assume the cells are in the correct order..
-    for cell in test_graph.cells:
-        await runner.run_cell(cell)
-
-
-asyncio.run(main())
 
 origins = ["*"]
 
@@ -68,20 +58,40 @@ app.add_middleware(
 @app.get("/read_graph")
 def read_graph():
     """Returns the serialised graph."""
-    with open("test_dag.gnote", "rb") as f:
-        dag = f.read()
-        return Response(dag)
+    return Response(test_graph.SerializeToString())
 
+class Item(BaseModel):
+    b64_graph: str
+
+
+
+# TODO: open websocket connection for continual updates from the kernel.
 
 @app.post("/save_graph")
-def save_graph():
+def save_graph(body: Item):
     """Compile & save the dag, returns updated proto (with latest in/out)."""
-    with open("test_dag.gnote", "rb") as f:
-        dag = f.read()
-        return Response(dag)
+    
+    req_graph = graph_pb2.Graph()
+    graph_bytes = base64.b64decode(body.b64_graph)
+    req_graph.ParseFromString(graph_bytes)
+
+    for cell in req_graph.cells:
+        detect_in_ports(cell)
+
+    # TODO: actually save the updated DAG and not just update state.
+    global test_graph
+    test_graph = req_graph
+    return Response(req_graph.SerializeToString())
 
 
 @app.get("/run_cell")
-def run_cell(cell_id: str):
+async def run_cell(cell_uid: str):
     """Runs the cell & returns updated cell (with output)"""
-    return True
+    global test_graph
+
+    for cell in test_graph.cells:
+        if cell.uid == cell_uid:
+            await runner.run_cell(cell)
+
+    # TODO: update output ports
+    return Response(test_graph.SerializeToString())
